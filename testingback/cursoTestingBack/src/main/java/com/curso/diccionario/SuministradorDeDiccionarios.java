fc018ed64d4d48527a5f03294811d52537358d0c
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2023-10-13 12:09:42 +0200

    Delete testingback/cursoTestingBack/src directory

--- testingback/cursoTestingBack/src/main/java/com/curso/diccionario/SuministradorDeDiccionarios.java
+++
@@ -1,8 +0,0 @@
-package com.curso.diccionario;
-
-import java.util.Optional;
-
-public interface SuministradorDeDiccionarios {
-    boolean tienesDiccionarioDe(String idioma);
-    Optional<Diccionario> getDiccionario(String idioma);
-}
